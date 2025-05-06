9d9b0cf48e544c9ac4a719c79b60736932365dd5
TALES DE SOUZA LOURENÇO <20231011110012@ifrn.local>
2025-05-06 14:13:50 -0300

    Alteração Admin

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
+    empty_value_display = 'Vazio'
 
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
