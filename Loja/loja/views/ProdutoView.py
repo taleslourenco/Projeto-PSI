6fc077ded003ea8a48d2188f766c0154d52107cf
TALES DE SOUZA LOURENÇO <20231011110012@ifrn.local>
2025-05-27 14:12:42 -0300

    implementação templates

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
@@ -10,12 +18,8 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
     if produto is not None:
-        produtos = produtos.filter(Produto__contains=produto )
+        produtos = produtos.filter(Produto__contains=produto)
     if promocao is not None:
         produtos = produtos.filter(promocao=promocao)
     if destaque is not None:
@@ -23,8 +27,36 @@ def list_produto_view(request, id=None):
     if categoria is not None:
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
-            produtos = produtos.filter(fabricante__Fabricante=fabricante)
-    if id is not None:
-        produtos = produtos.filter(id=id)
-    print(produtos)
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    # Adicione para definir o contexto e carregar o template
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html', context=context, status=200)
+def edit_produto_postback(request, id=None):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+                obj_produto.save()
+                print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+                print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
