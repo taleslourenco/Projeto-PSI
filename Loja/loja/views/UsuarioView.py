5c0b0be4b9ec8670b46060cdf3b1a3e499804dbb
TALES DE SOUZA LOURENÇO <20231011110012@ifrn.local>
2025-08-11 15:46:57 -0300

    edit usuário modificado

--- Loja/loja/views/UsuarioView.py
+++ Loja/loja/views/UsuarioView.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
-from loja.forms.UserUsuarioForm import UserUsuarioForm
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
     # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
@@ -11,7 +11,9 @@ def list_usuario_view(request, id=None):
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
     usuarioForm = UserUsuarioForm(instance=usuario)
+    userForm = UserForm(instance=request.user)
     context = {
-        'usuarioForm': usuarioForm
+        'usuarioForm': usuarioForm,
+        'userForm': userForm
     }
     return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
